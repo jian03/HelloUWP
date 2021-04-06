8519498cd82496feca80a52f5a9e286508c2a792
Jian Choi <s2019s19@e-mirim.hs.kr>
2021-04-07 08:48:50 +0900

    초기 커밋.

---
+++ HelloUWP/Views/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+
+namespace HelloUWP.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var myDlg = new MessageDialog("안녕하");
+            myDlg.Commands.Add(new UICommand("OK"));
+            myDlg.Commands.Add(new UICommand("안OK"));
+            myDlg.Commands.Add(new UICommand("싫음"));
+            await myDlg.ShowAsync();
+        }
+    }
+}
